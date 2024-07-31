be47344de900bb056e970981be88926687adeab7
roger <roger123593@gmail.com>
2024-07-31 11:14:41 +0800

    q1~q6

--- Concurrency.py
+++ Concurrency.py
@@ -19,12 +19,12 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
-    thread_1 = threading.Thread(target=sleep_5)  # 例項化一個執行緒物件，使執行緒執行這個函式
-    thread_2 = threading.Thread(target=sleep_10)  # 例項化一個執行緒物件，使執行緒執行這個函式
-    thread_1.start()  # 啟動這個執行緒
-    thread_2.start()  # 啟動這個執行緒
-    thread_1.join()  # 等待thread_1結束，如果不打join程式會直接往下執行
-    thread_2.join()  # 等待thread_2結束，如果不打join程式會直接往下執行
+    thread_1 = threading.Thread(target=sleep_5)
+    thread_2 = threading.Thread(target=sleep_10)
+    thread_1.start()
+    thread_2.start()
+    thread_1.join()  # 等待thread_1結束
+    thread_2.join()  # 等待thread_2結束
 
     end_time = time.time()
     print('It costs ' + str(end_time - start_time) + ' s')
